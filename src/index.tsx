f2ad59314637b963627525e78a20d6acd3dfd69a
webofceco <mapleselijah9@gmail.com>
2024-10-01 13:53:16 -0400

    update styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
-import "./assets/stylus/index.styl";
+import "./assets/css/index.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container missing in index.html");
